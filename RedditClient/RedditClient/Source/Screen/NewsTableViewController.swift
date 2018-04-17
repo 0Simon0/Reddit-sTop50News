63de71a57ff207f7144bc2d853705a1e4599c313
Yana Voloskova <yana.voloskova@petcube.com>
2018-04-17 16:07:37 +0300

    Initial project

---
+++ RedditClient/RedditClient/Source/Screen/NewsTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewsTableViewController.swift
+//  RedditClient
+//
+//  Created by Yana VV on 4/17/18.
+//  Copyright © 2018 YV. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+}
