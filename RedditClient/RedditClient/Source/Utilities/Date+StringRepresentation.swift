d8f33c5f1397cde02cb99aa7a3af223e0af55e0f
Yana VV <yanaVV@Yanas-Mac.none>
2018-04-25 18:35:55 +0300

    Fix converting date to a format like “x hours ago”

--- RedditClient/RedditClient/Source/Utilities/Date+StringRepresentation.swift
+++ RedditClient/RedditClient/Source/Utilities/Date+StringRepresentation.swift
@@ -11,24 +11,35 @@ import Foundation
 extension Date {
 
 	static func stringRepresentationOfTime(sinceDate date: Date) -> String {
+
 		let diff = Int(Date().timeIntervalSince1970 - date.timeIntervalSince1970)
-		if diff < 3600 {
+		guard diff >= 60 * 60 else {
 			let minutes = diff / 60
 			return "\(minutes) \(minutes == 1 ? "minute" : "minutes") ago"
 		}
-		if diff < 3600 * 24 {
-			let hours = diff / 3600
+		guard diff >= 60 * 60 else {
+			let minutes = diff / 60
+			return "\(minutes) \(minutes == 1 ? "minute" : "minutes") ago"
+		}
+		guard diff >= 60 * 60 * 24 else {
+			let hours = diff / (60 * 60)
 			return "\(hours) \(hours == 1 ? "hour" : "hours") ago"
 		}
-		if diff < 3600 * 24 * 31 {
-			let days = diff / 3600 / 24
-			return "\(days) days ago"
+		guard diff >= 60 * 60 * 24 * 7 else {
+			let days = diff / (60 * 60 * 24)
+			return "\(days) \(days == 1 ? "day" : "days") ago"
 		}
-		if diff < 3600 * 24 * 31 * 365 {
-			let months = diff / 3600 / 24 / 31
+		guard diff >= 60 * 60 * 24 * 31 else {
+			let weeks = diff / (60 * 60 * 24 * 7)
+			return "\(weeks) \(weeks == 1 ? "week" : "weeks") ago"
+		}
+
+		let dateComponents = Calendar.current.dateComponents([.year, .month], from: date, to: Date())
+		let years = dateComponents.year!
+		guard years > 0 else {
+			let months = dateComponents.month!
 			return "\(months) \(months == 1 ? "month" : "months") ago"
 		}
-		let years = diff / 3600 / 365
 		return "\(years) \(years == 1 ? "year" : "years") ago"
 	}
 }
